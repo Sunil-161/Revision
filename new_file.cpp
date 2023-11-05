868d778d16d83e4911eb5cb60a01329b12b2655f
Sumit More <gusunil78@gmail.com>
2023-11-05 20:29:37 +0530

    created new_file.cpp

---
+++ new_file.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class sample{
+	private :
+	string temp;
+	string roll_no;
+
+	public :
+	sample(): temp("NONE"), roll_no("0")
+	{
+	}
+	get_Data()
+	{
+
+		cin >> temp ;
+		cin >> roll_no;
+		}
+		
+};
+
+int main()
+{
+		cout << "Hello world\n";
+
+	return 0;
+}
+
